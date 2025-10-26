ac39cdd49580f0a47d880a891debe14d64c2c0bf
welldotdocx <razaelsaputra863@gmail.com>
2025-10-25 22:39:07 -0700

    Remove project configuration and assets
    - Deleted package.json, postcss.config.mjs, and tsconfig.json to remove project dependencies and configurations.
    - Removed SVG assets from the public directory, including file.svg, globe.svg, next.svg, vercel.svg, and window.svg.

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <div className="justify-center items-center flex min-h-screen flex-col">
-      <h1 className=" text-center">Welcome to my portfolio</h1>
-    </div>
-  );
-}
--- next.config.ts
+++
@@ -1,7 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  allowedDevOrigins: ['http://139.59.101.100:3000']
-};
-
-export default nextConfig;
